c2d043f10b281f42922a3d2bf1deae1bc5dbefe1
mattbeard03 <87513616+mattbeard03@users.noreply.github.com>
2024-09-15 07:34:47 +0000

    Added light-primary colour, removed 'hero' class, merging home page elements into 'page.tsx' only. Code Runs, and all home page items are easier to edit now.

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,23 +1,51 @@
-import { Categories } from '@/categories/categories';
 import { Container } from '@/common/container';
-import { Hero } from '@/common/hero';
+import {APP_DESCRIPTION,APP_TITLE,} from '@/common/common-utils';
+import { routes } from '@/routing/routing-utils';
+import { ButtonLink } from '@/common/button-link';
+import { Divider } from '@/common/divider';
 import { getMetadata } from '@/seo/seo-utils';
 import Image from 'next/image';
-export const metadata = getMetadata({ title: 'Home', pathname: '/' });
 import mainpicture from '@/app/mainpic.png';
+
+export const metadata = getMetadata({ title: 'Home', pathname: '/' });
 
 export default function LandingPage() {
   return (
     <main>
-      <Hero />
+      <div className="border-b">
+        <div className="flex flex-col items-center gap-4 px-4 py-12 text-center">
+          <h1 className="text-5xl font-black text-primary sm:text-7xl lg:text-8xl">
+            {APP_TITLE}
+          </h1>
+          <Divider />
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-lg font-semibold text-muted-foreground sm:text-xl">
+              {APP_DESCRIPTION}
+            </p>
+          </div>
+
+          <div className="flex gap-4">
+            <ButtonLink variant="primary" href={routes.search()}>
+              Learn More
+            </ButtonLink>
+            <ButtonLink variant="primary" href={routes.search()}>
+              Explore
+            </ButtonLink>
+          </div>
+        </div>
+      </div>
+
       <Container maxWidth="xl" className="p-4">
-       <div>
-        <Image 
-        src={mainpicture} alt='Description'
-        width={1000}
-        height ={300} />
-       </div>
+        <div>
+          <Image 
+            src={mainpicture} 
+            alt='Description'
+            width={1000}
+            height={300} 
+          />
+        </div>
       </Container>
     </main>
   );
 }
+
