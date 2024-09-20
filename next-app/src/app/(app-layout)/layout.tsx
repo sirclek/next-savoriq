932bc2595462f9a7dc2e7180805fac6aaac1c2c9
Kelvin <kelvinsofjan@gmail.com>
2024-09-20 16:12:25 +1000

    separated learn more page, removed cart-drawer stuff because redundant and causing errors.

--- next-app/src/app/(app-layout)/layout.tsx
+++ next-app/src/app/(app-layout)/layout.tsx
@@ -1,7 +1,5 @@
-import { LayoutContent } from '@/layout/layout';
 
 type AppLayoutProps = React.PropsWithChildren;
 
 export default function AppLayout({ children }: AppLayoutProps) {
-  return <LayoutContent>{children}</LayoutContent>;
 }
--- next-app/src/routing/routing-utils.ts
+++ next-app/src/routing/routing-utils.ts
@@ -66,8 +66,8 @@ export const routes = {
     (params) => `/products/${params.productId}`,
   ),
   // routing-utils.ts
-  explore: createRoute(() => '/explore'), 
-  checkout: createRoute(() => '/checkout'),
+  explore: createRoute(() => '/explore'),
+  learnmore: createRoute(() => '/learnmore'),
 };
 
 // TODO: Will check the use cases for this.
