67a4017f8fa3e7d63130b6988bfa9d510605dab2
mattbeard03 <87513616+mattbeard03@users.noreply.github.com>
2024-10-26 12:31:37 +0000

    remove optimistic filter context

--- next-app/src/app/(app-layout)/search/page.tsx
+++ next-app/src/app/(app-layout)/search/page.tsx
@@ -3,14 +3,6 @@ import { Section, SectionTitle } from '@/common/section';
 import { WhiskeyFilter } from '@/search/whiskey-filter';
 import { filterProducts } from '@/search/search-fetchers';
 import { SearchResults } from '@/search/search-results';
-import { SelectedFilters } from '@/search/selected-filters';
-import { SelectedOptionsProvider } from '@/search/selected-options-context';
-import { getMetadata } from '@/seo/seo-utils';
-
-export const metadata = getMetadata({
-  title: 'Search Whiskeys',
-  pathname: '/search',
-});
 
 type SearchPageProps = {
   searchParams: Record<string, string | string[]>;
@@ -22,7 +14,6 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
   return (
     <main className="group/page">
       <PageTitle title="Search Whiskeys" srOnly />
-      <SelectedOptionsProvider data={data}>
         <div className="grid gap-2 md:grid-cols-[theme(spacing.72)_1fr]">
           <Section className="sticky top-24 hidden max-h-[80vh] overflow-auto px-2 md:block">
             <SectionTitle as="h2" srOnly>
@@ -36,12 +27,10 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
               Search Results
             </SectionTitle>
             <div className="flex flex-col gap-2">
-              <SelectedFilters />
               <SearchResults data={data} />
             </div>
           </Section>
         </div>
-      </SelectedOptionsProvider>
     </main>
   );
 }
