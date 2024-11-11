4d305ddec2509118bbcc3dae69042568b99fa047
ngu892 <66870782+ngu892@users.noreply.github.com>
2024-11-11 22:25:39 +1100

    Update href from whisky page to flavour page

--- next-app/src/app/(app-layout)/flavours/[flavourId]/page.tsx
+++ next-app/src/app/(app-layout)/flavours/[flavourId]/page.tsx
@@ -1,6 +1,7 @@
 import type { Flavour, Id } from '@/common/custom-types';
 import { Paper } from '@/common/paper';
-import { Section } from '@/common/section';
+import { PageTitle } from '@/common/page-title';
+import { Section, SectionTitle } from '@/common/section';
 import { dataTypes, getObjectById } from '@/db/db-utils';
 import { getMetadata } from '@/seo/seo-utils';
 import type { Metadata } from 'next';
@@ -30,7 +31,8 @@ export default async function FlavourPage({ params }: FlavourPageProps) {
     <>
       <Section>
         <Paper>
-          <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
+        <PageTitle title={flavour.name} />
             <div className="grid gap-6 md:grid-cols-2">
               <div className="relative mx-auto aspect-square w-full max-w-sm md:max-w-lg">
                 <Image className="rounded bg-white object-contain" src={`/images/flavours/${flavour.id}.png`} alt={flavour.name} priority fill />
@@ -54,9 +56,21 @@ export default async function FlavourPage({ params }: FlavourPageProps) {
             </div>
           </div>
         </Paper>
+        
+      </Section>
+      <Section as="aside">
+        <SectionTitle as="h2">Related by Flavour</SectionTitle>
+       <Paper>
+        <p>Related by 2</p>
+        </Paper>
       </Section>
 
-      <Section></Section>
+      <Section as="aside">
+        <SectionTitle as="h2">Related by Chemicals</SectionTitle>
+       <Paper>
+        <p>Related by 3</p>
+        </Paper>
+      </Section>
     </>
   );
 }
