4bf7b2ba8fd4a0c155ce9407a1671849d500604d
Kelvin <kelvinsofjan@gmail.com>
2024-11-17 22:55:42 +1100

    bug fix

--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -14,9 +14,6 @@ export default {
       fontFamily: {
         sans: ['var(--font-inter)'],
       },
-      spacing: {
-        '0.9': '0.9rem',
-      },
       colors: {
         background: 'hsl(var(--background) / <alpha-value>)',
         foreground: 'hsl(var(--foreground) / <alpha-value>)',
